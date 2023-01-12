daae12059a656949e7f157b0517de7da189e7442
Qvezal <andrey.kazako75@gmail.com>
2023-01-12 21:59:57 +0300

    i'm here

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import { Inter } from "@next/font/google";
 import Container from "@/components/std/Container";
 import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
         <Container wrapper>
             <h1>Hi mom</h1>
+            <h2>HIHI</h2>
         </Container>
     );
 }
