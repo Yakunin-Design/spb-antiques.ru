a62ec39904ae3adb8bdce7558a427e1fbda33652
Yakunin Egor <e.yakunin04@gmail.com>
2023-04-16 14:41:25 +0000

    stats server connected

--- lib/incoming_send.ts
+++ lib/incoming_send.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function incoming_send(source?: string) {
     axios
-        .post("http://localhost:8080/add_source", {
+        .post("https://stats.yakunin.io/add_source", {
             project: "spb-antiques",
             source: source === "ya" ? "ya" : "other",
         })
